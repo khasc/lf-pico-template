b8776c0b3a7ee20d76256304b51be1df5d5513c2
khasc <200988182+khasc@users.noreply.github.com>
2026-02-16 10:47:35 -0500

    Update shell.nix
    show how to explicitly set Pico environment variables, use lolcat to colorize notable Pico settings in shell output, added shell message to tell how to exit the shell made by nix develop

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ let
 in 
 pkgs.mkShell {
   packages = with pkgs; [
+    lolcat
     picotool
     cmake
     gcc-arm-embedded
@@ -20,6 +21,10 @@ pkgs.mkShell {
     # lingo
   ];
 
+  # set environment variables
+  #PICO_BOARD = "pico";
+  #PICO_PLATFORM = "rp2040";
+
   # TODO: integrate dependencies into nix
   shellHook = ''
     echo "[shell] hook start"
@@ -28,13 +33,17 @@ pkgs.mkShell {
     cd pico-sdk/
     git submodule update --init
     export PICO_SDK_PATH="$PWD"
+    echo "[shell] PICO_SDK_PATH: $PICO_SDK_PATH" | lolcat
     cd ../
-    echo "[shell] copy robot header"
+    echo "[shell] copy robot header (pololu_3pi_2040_robot.h)" | lolcat
     cp pololu_3pi_2040_robot.h pico-sdk/src/boards/include/boards/
+    echo "[shell] PICO_BOARD: $PICO_BOARD" | lolcat
+    echo "[shell] PICO_PLATFORM: $PICO_PLATFORM" | lolcat
     echo "[shell] setup testbed"
     cd test/
     npm install
-    echo "[shell] hook end"
     cd ../
+    echo '[shell] To exit the shell, type `exit` or press `Ctrl`+`D`'
+    echo "[shell] hook end"
     '';
 }
